a75f7614e2e40e484a42bc122244937d2e117b93
Enki <39771190+Enki013@users.noreply.github.com>
2023-06-12 16:07:23 +0300

    1.2
    klasörden veri okuma ve yazma eklendi

--- decrypt_file.py
+++ decrypt_file.py
@@ -1,6 +1,8 @@
 import cryptography
 from cryptography.fernet import Fernet
+import os
 
+# Anahtar seçeneği
 key_choice = input("Anahtarı kendiniz mi belirlemek istiyorsunuz? (E/H): ")
 
 if key_choice.upper() == 'H':
@@ -9,13 +11,24 @@ if key_choice.upper() == 'H':
         key=mykey.read()
 else:
     key = input("Lütfen anahtar metninizi girin: ").encode()
+    
+# Klasöre gitme ve dosya listeleme
+current_directory = os.getcwd()
+folder = os.path.join(current_directory, 'encrypted')
+file_list = os.listdir(folder)
 
-# Dosyanın Şifresini Çözme
+# Dosyaları Şifresini Çöz
 f = Fernet(key)
-with open('enc_grades.csv', 'rb') as encrypted_file:
-    encrypted = encrypted_file.read()
+for filename in file_list:
+    with open(os.path.join(folder,filename), 'rb') as encrypted_file:
+        encrypted = encrypted_file.read()
+        try: 
+            decrypted = f.decrypt(encrypted)
+        except cryptography.fernet.InvalidToken:
+            continue
+         # Dosya şifrelenmemişse pass geçmek için
+        decrypted_filename = 'dec_' + filename.replace('enc_', '')
+        with open(os.path.join(current_directory, 'decrypted', decrypted_filename), 'wb') as decrypted_file:
+            decrypted_file.write(decrypted)
 
-    decrypted = f.decrypt(encrypted)
-
-    with open('dec_grades.csv', 'wb') as decrypterd_file:
-        decrypterd_file.write(decrypted)
+print("Bitti!")
--- encrypt_file.py
+++ encrypt_file.py
@@ -1,28 +1,48 @@
-import cryptography;
-
-#Anahtar Oluşturma
+import os
 from cryptography.fernet import Fernet
 
-key=Fernet.generate_key()
+# Anahtar Oluşturma
+key = Fernet.generate_key()
 
-with open('mykey.key','wb') as mykey:
-        mykey.write(key)
+with open('mykey.key', 'wb') as mykey:
+    mykey.write(key)
 
-        
-#Anahtar Yükleme
-with open('mykey.key','rb') as mykey:
-    key=mykey.read()
+
+# Anahtar Yükleme
+with open('mykey.key', 'rb') as mykey:
+    key = mykey.read()
 
     print(key)
 
 
-# Dosyayı Şifrelemek
-f=Fernet(key)
-with open('grades.csv','rb') as original_file:
-      original=original_file.read()
+# Tüm Dosyaları Şifreleme
 
-encrypted=f.encrypt(original)
 
-with open ('enc_grades.csv','wb') as encrypted_file:
-      encrypted_file.write(encrypted)
+f = Fernet(key)
 
+# Klasör yolu
+current_directory = os.getcwd()
+folder_path = os.path.join(current_directory, 'files')
+print(folder_path)
+#Dosya adlarını al
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+
+    #Eğer .py veya .key dosyası değilse
+    if not (filename.endswith('.py') or filename.endswith('.key')):
+
+        #Dosya şifreleme
+        with open(file_path, 'rb') as original_file:
+            original = original_file.read()
+        encrypted = f.encrypt(original)
+
+        #Şifrelenmiş dosyaları encrypted klasörüne kaydet
+        encrypted_path = os.path.join("encrypted", f'enc_{filename}')
+        with open(encrypted_path, 'wb') as encrypted_file:
+            encrypted_file.write(encrypted)
+
+        print(f'{filename} şifrelendi.')
+    else:
+        print(f'{filename} şifrelenmedi.')
+
+print('Tüm dosyalar şifrelendi.')
